5b5c2e554b67f94724886b4fbc13869bebe284e1
Vikram Kumar <vikramraimumbai@gmail.com>
2022-03-07 01:12:42 +0530

    TODO with authentication

--- server.js
+++ server.js
@@ -5,23 +5,34 @@ const session = require('express-session');
 const passport = require('passport');
 const bodyparser = require("body-parser");
 const cors = require("cors");
-
+//port
+const port = process.env.PORT || 3001; 
 //passport
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 const GOOGLE_CLIENT_ID = process.env.clientID;
 const GOOGLE_CLIENT_SECRET = process.env.clientSecret;  
-
+if(port === 3000){
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/google/callback",
+  callbackURL: "http://localhost:3000/google/callback" ,
   passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
   return done(null, profile);
 }));
-
+}else{
+  passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://todo-app-monoceros.herokuapp.com/google/callback" ,
+    passReqToCallback: true,
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }));
+}
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -52,7 +63,7 @@ app.use(passport.session());
 require("./routes/todo")(app);
 require("./routes/user")(app);
 
-const port = process.env.PORT || 3001; 
+
 //mongodb connect
 const db = require("./model");
 const Mongourl = process.env.MONGODB_URL;
